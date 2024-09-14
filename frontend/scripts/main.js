c0ef6ee84962067aa3dfc025481c50e16e792a25
yeraldin-cano-nexer <148908208+yeraldin-cano-nexer@users.noreply.github.com>
2024-09-14 09:27:38 -0500

    Front 5

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -50,8 +50,61 @@ window.onclick = function(event) {
     }
 }
 
+function addList(item, itemList){
+    var profile= capitalizeFirstLetter(item.profile_type);
+    const li = document.createElement('li');
+    li.id=profile;
+    li.textContent = profile;
+    const div=document.createElement('div')
+    div.className="buttons"
+    const btn1 = document.createElement('button');
+    btn1.className="view-button"
+    const btn2 = document.createElement('button');
+    btn2.className="edit-button"
+    btn1.setAttribute('onclick', "verElemento("+profile+")");
+    btn2.setAttribute('onclick', "editarElemento("+profile+")");
+    btn1.textContent = "View";
+    btn2.textContent = "Delete"
+    btn1.id = "button Ver"+ profile ;
+    btn2.id = "button Ver"+ profile ;
 
-function fetchItems() {
+    btn1.append;
+    btn2.append;
+    div.appendChild(btn1);
+    div.appendChild(btn2);
+
+    div.append
+    li.appendChild(div);
+    li.append
+    itemList.appendChild(li);
+}
+
+function addDropdown(item, div_drop){
+    var profile= capitalizeFirstLetter(item.profile_type);
+    const label = document.createElement('label');
+    const input= document.createElement('input');
+    input.type="checkbox";
+    input.name="button name " + profile;
+    input.value="button value "+ profile;
+    
+    input.append
+    label.appendChild(input);
+    label.innerHTML += profile;    
+
+    label.append;
+    
+    div_drop.appendChild(label);
+
+}
+
+function capitalizeFirstLetter(word) {
+    if (word.length === 0) return ''; // Retorna una cadena vacía si la palabra está vacía
+   const palabra= word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+   
+    return palabra
+}
+
+function getProfiles() {
     fetch('http://localhost:3000/profiles')
         .then(response => response.json())
         .then(data => {
@@ -60,54 +113,12 @@ function fetchItems() {
             itemList.innerHTML = ''; // Limpiar lista
             
             data.profiles.forEach(item => {
-                const li = document.createElement('li');
-                
-                //add li en profile
-                li.id=item.profile_type;
-                li.textContent = item.profile_type;
-                const div=document.createElement('div')
-                div.className="buttons"
-                const btn1 = document.createElement('button');
-                btn1.className="view-button"
-                const btn2 = document.createElement('button');
-                btn2.className="edit-button"
-                btn1.setAttribute('onclick', "verElemento("+item.profile_type+")");
-                btn2.setAttribute('onclick', "editarElemento("+item.profile_type+")");
-                btn1.textContent = "Ver";
-                btn2.textContent = "Editar"
-                btn1.id = "button Ver"+ item.profile_type ;
-                btn2.id = "button Ver"+ item.profile_type ;
-
-                btn1.append;
-                btn2.append;
-                div.appendChild(btn1);
-                div.appendChild(btn2);
-
-                div.append
-                li.appendChild(div);
-                li.append
-                itemList.appendChild(li);
-                
-
-                //add label en dropdown
-
-                const label = document.createElement('label');
-                const input= document.createElement('input');
-                input.type="checkbox";
-                input.name="button name " + item.profile_type;
-                input.value="button value "+ item.profile_type;
-                
-                input.append
-                label.appendChild(input);
-                label.innerHTML += item.profile_type;    
-
-                label.append;
-                
-                div_drop.appendChild(label);
-
+                addList(item, itemList);
+                addDropdown(item, div_drop);
+               
             });
         });
 }
 
 // Obtener datos al cargar la página
-window.onload = fetchItems;
+window.onload = getProfiles;
