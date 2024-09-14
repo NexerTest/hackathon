8df358496b8cba2891bbafb2923b8d83f4bd606e
yeraldin-cano-nexer <148908208+yeraldin-cano-nexer@users.noreply.github.com>
2024-09-14 07:56:07 -0500

    Front 3

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -59,8 +59,33 @@ function fetchItems() {
             itemList.innerHTML = ''; // Limpiar lista
             data.profiles.forEach(item => {
                 const li = document.createElement('li');
+                li.id=item.profile_type;
                 li.textContent = item.profile_type;
+                
+                const div=document.createElement('div')
+                div.className="buttons"
+                const btn1 = document.createElement('button');
+                btn1.className="view-button"
+                const btn2 = document.createElement('button');
+                btn2.className="edit-button"
+                btn1.setAttribute('onclick', "verElemento("+item.profile_type+")");
+                btn2.setAttribute('onclick', "editarElemento("+item.profile_type+")");
+                btn1.textContent = "Ver";
+                btn2.textContent = "Editar"
+                btn1.id = "button Ver"+ item.profile_type ;
+                btn2.id = "button Ver"+ item.profile_type ;
+
+                btn1.append;
+                btn2.append;
+                div.appendChild(btn1);
+                div.appendChild(btn2);
+
+                div.append
+                li.appendChild(div);
+                li.append
                 itemList.appendChild(li);
+                
+
             });
         });
 }
